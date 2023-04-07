76f4e7cc10a8bf292b2ca0bed89e2ec85aec59ac
soni2261 <61927635+soni2261@users.noreply.github.com>
2023-04-07 12:59:36 -0400

    added field requirements

--- apply_membership.js
+++ apply_membership.js
@@ -1,36 +1,13 @@
-let form = document.getElementById('form');
-const submit = document.getElementById('submitButton');
-let firstName,lastName,age,address,postalCode;
-
-const urlParams = new URLSearchParams(window.location.search);
-const message = urlParams.get('msg');
-
-if ( message != null ){
-    const input = document.createElement("h6");
-    input.innerHTML = message;
-    document.body.insertBefore(input,document.getElementById("form"));
-}
-
-submit.addEventListener("click", () => {
-    firstName = document.getElementById('firstName')
-    lastName = document.getElementById('lastName')
-    age = document.getElementById('age')
-    address = document.getElementById('address')
-    postalCode = document.getElementById('postalCode')
-
-    if (firstName.value == '') {
-        alert("Please add a first name")
-    } else if (lastName.value == '') {
-        alert("Please add a last name")
-    } else if (age.value == '') {
-        alert("Please add an age")
-    } else if (address.value == ''){
-        alert("Please add an address")
-    } else if (postalCode.value == ''){
-        alert("Please add a postal code")
-    } else {
-        form.submit();
-        document.location = 'success.html'
+function selectOne() {
+    var select = document.getElementById('age');
+    select.options[0] = new Option();
+    select.options[0].id="age-select"
+    var ageselect = document.getElementById("age-select");
+    ageselect.setAttribute("hidden", true);
+    ageselect.setAttribute("disabled", false);
+    ageselect.setAttribute("selected", false);
+    for (var i=10; i<=100; i++) {
+        select.options[select.options.length] = new Option(i, i);
     }
-
-});
+}
+selectOne();
